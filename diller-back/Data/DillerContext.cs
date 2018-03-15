a330422a6850900962e6014b6e7544ae6a826958
Konstantin <konstantinvasin747@gmail.com>
2018-03-15 21:08:46 +0200

    first commit

---
+++ diller-back/Data/DillerContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Diller.Models
+{
+    public class DillerContext : DbContext
+    {
+        public DillerContext (DbContextOptions<DillerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AutoBrand> AutoBrand { get; set; }
+    }
+}
