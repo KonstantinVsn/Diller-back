7ccd2b0cef7151953ea8274431d301968c8a6a99
Konstantin <konstantinvasin747@gmail.com>
2018-05-13 20:10:07 +0300

    added cars

---
+++ diller-db/dbo.AutoCategories.data.sql
@@ -0,0 +1,5 @@
+SET IDENTITY_INSERT [dbo].[AutoCategories] ON
+INSERT INTO [dbo].[AutoCategories] ([Id], [Name]) VALUES (1, N'pickup')
+INSERT INTO [dbo].[AutoCategories] ([Id], [Name]) VALUES (2, N'sedan')
+INSERT INTO [dbo].[AutoCategories] ([Id], [Name]) VALUES (3, N'jeep')
+SET IDENTITY_INSERT [dbo].[AutoCategories] OFF
---
+++ diller-db/dbo.Cars.data.sql
@@ -0,0 +1,3 @@
+SET IDENTITY_INSERT [dbo].[Cars] ON
+INSERT INTO [dbo].[Cars] ([Id], [BrandId], [CategoryId], [Color], [EnVolume], [Height], [Length], [MaxSpeed], [Width], [Year]) VALUES (NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
+SET IDENTITY_INSERT [dbo].[Cars] OFF
